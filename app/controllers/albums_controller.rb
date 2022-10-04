07e9592386160151f6a92046a6685f841acf7c4e
masa0501 <nakamura19980501@gmail.com>
2022-10-04 14:46:21 +0900

    複数枚投稿の実装

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -37,6 +37,6 @@ class AlbumsController < ApplicationController
   private
 
   def album_params
-    params.require(:album).permit(:name, :image)
+    params.require(:album).permit(:name, {images: []})
   end
 end
