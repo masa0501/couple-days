7a6d36344fac9870e3896ecd828b9a79b6533944
masa0501 <nakamura19980501@gmail.com>
2022-10-07 14:10:09 +0900

    メモ機能の保存と表示

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,19 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
+    @event = Event.new
   end
 
   
+  def create
+    Event.create(event_params)
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :content, :start_time)
+  end
+
 end
