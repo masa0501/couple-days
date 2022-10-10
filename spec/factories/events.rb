51f0e072b73002840f7ec876771f4f9985b27254
masa0501 <nakamura19980501@gmail.com>
2022-10-10 11:45:03 +0900

    eventモデルのテスト

---
+++ spec/factories/events.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :event do
+    title              {'タイトル'}
+    content            {'内容'}
+    start_time         {Time.new.strftime("%H:%M")}
+
+    association :user
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {'test'}
     partner_nickname      {'test'}
-    dating_day            {'2014-04-30'}
+    dating_day            {'2000-01-01'}
     email                 {'test@example'}
     password              {'000000'}
     password_confirmation {password}
